e5ce6dd4243ee2cc8ed71d0611d9bb6134a0c5cd
Graham Dyson <gdyson@williamhill.us>
2024-12-05 13:05:26 +0000

    many things

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 brew install --cask alt-tab
 brew install --cask betterdisplay
-brew install --cask graphviz
 brew install --cask grep
-brew install --cask iterm2
 brew install --cask karabiner-elements
+brew install --cask kcat
 brew install --cask linearmouse
-brew install --cask monitorcontrol
-brew install --cask pinta
-brew install --cask pinniped-cli 
+brew install --cask parallel
+brew install --cask pinniped-cli
 brew install --cask rectangle
+# Talon
